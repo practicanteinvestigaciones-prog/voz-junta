7d6a82a7a249e098baa5a1451be74d82adc0d421
practicanteinvestigaciones-prog <practicanteinvestigaciones@cccartagena.org.co>
2026-09-24 03:22:57 +0000

    Update build.py

--- panel/build.py
+++ panel/build.py
@@ -5,7 +5,8 @@
 s = open('original.html', encoding='utf-8').read()
 css = open('voz.css', encoding='utf-8').read()
 html = open('voz.html', encoding='utf-8').read()
-js = open('voz.js', encoding='utf-8').read()
+logos = open('logos.js', encoding='utf-8').read()
+js = logos + open('voz.js', encoding='utf-8').read()
 nav = ('<button class="nv" data-v="voz"><svg viewBox="0 0 24 24">'
        '<rect x="9" y="3" width="6" height="11" rx="3"/>'
        '<path d="M5 11a7 7 0 0 0 14 0M12 18v3"/></svg>'
